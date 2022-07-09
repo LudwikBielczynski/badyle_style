32e0e8ba803b553d2280fcc499ca2e5f2db79653
Ludwik Bielczynski <ludwik.bielczynski@yieldplanet.com>
2022-07-09 22:29:33 +0200

    Added navigation tabs and title to the main page

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,20 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { TabsNavigationComponent } from './tabs-navigation/tabs-navigation.component';
+import { MatTabsModule } from '@angular/material/tabs'
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    TabsNavigationComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    MatTabsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
